6d9b394fe263199c072c1c0a24dba493d07db3e8
ossama assaghir <59144753+ossamasgr@users.noreply.github.com>
2022-07-24 20:17:42 +0100

    Update copyvhd.ps1

--- copyvhd.ps1
+++ copyvhd.ps1
@@ -34,32 +34,19 @@
 
         [Parameter(Mandatory=$true)]
         [string] 
-        $sourceSasToken,
-
+        $sourceSasToken
         [Parameter(Mandatory=$true)]
         [string] 
-        $SourceStorageAccountKey
+        $ResourceGroupName
 )
 
         # Credentials
         $myCredential = Get-AutomationPSCredential -Name 'automationCredentials'
-		$userName = $myCredential.UserName
-		$securePassword = $myCredential.Password
-		##Destination Storage Account : 
-        #$dest_storageAccountName = "stgnsstst0012"
-		#$destContainerName = "zscalernssrprod"
-		#$destStorageAccountKey = 'YdRtwa2pu5hRhD77D2xNnlfly5lJ1UeL3NUz/ILhaARS9eghjdQ9uojwyO8XZSiBIdrQEBA6yY+Q+AStnyRdpA=='        
-		$destContext = New-AzStorageContext -StorageAccountName $dest_storageAccountName -StorageAccountKey $destStorageAccountKey
-        #$desteblob = "vhd.disk"
-        ##Source Storage Account (znss) : 
-        #$SourceStorageAccountName = "stgnsstst0012"
-        #$Sourcecontainer = "vhd"
-		#$sourceVHDname = "disk.vhd"
-		#$sourceVhdURL = "https://stgnsstst0012.blob.core.windows.net/vhd/disk.vhd?"
-		#$sourceSasToken = "sp=rwi&st=2022-07-24T15:43:23Z&se=2022-07-24T23:43:23Z&sv=2021-06-08&sr=b&sig=aToNYaxRqUkwY1RVRv6WZd3ksBE1aLzVbkProQQur70%3D"
-		$sasVHDurl=$sourceVhdURL+$sourceSasToken
-        #$SourceStorageAccountKey = 'YdRtwa2pu5hRhD77D2xNnlfly5lJ1UeL3NUz/ILhaARS9eghjdQ9uojwyO8XZSiBIdrQEBA6yY+Q+AStnyRdpA=='
-        $SourceContext = New-AzStorageContext -StorageAccountName $SourceStorageAccountName -StorageAccountKey $SourceStorageAccountKey
+        $userName = $myCredential.UserName
+	$securePassword = $myCredential.Password
+	$destStorageAccountKey = (Get-AzStorageAccountKey  -ResourceGroupName $ResourceGroupName -Name $Source_storageAccount).Value[0]        
+	$destContext = New-AzStorageContext -StorageAccountName $dest_storageAccountName -StorageAccountKey $destStorageAccountKey
+	$sasVHDurl=$sourceVhdURL+$sourceSasToken
 
 		
         echo 'start the copy'
