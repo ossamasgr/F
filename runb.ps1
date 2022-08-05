48fa5f76c1c4356eae3cc6119edb7c769b563820
ossama assaghir <59144753+ossamasgr@users.noreply.github.com>
2022-08-05 17:06:47 +0100

    Update runb.ps1

--- runb.ps1
+++ runb.ps1
@@ -9,18 +9,23 @@ param(
 
     [Parameter(Mandatory=$true)]
     [string] 
-    $ComputerName,
+    $IPName,
 
     [Parameter(Mandatory=$true)]
     [string] 
-    $scripturl
+    $scripturl,
+    [Parameter(Mandatory=$true)]
+    [string] 
+    $RG
 )
 
 $scripturl = 'https://raw.githubusercontent.com/ossamasgr/F/main/script.sh'
 $Command = "fetch $scripturl; sh script.sh"
 $secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
 $Credentials = New-Object System.Management.Automation.PSCredential($User, $secpasswd)
-
+$ComputerName = Get-AzPublicIpAddress -ResourceGroupName $RG -Name $IPName | Select-Object -ExpandProperty ipAddress
+echo 'ip is : '
+echo $ComputerName
 echo 'logging...'
 $SessionID = New-SSHSession -ComputerName $ComputerName -AcceptKey -Credential $Credentials 
 echo 'Exucuting...'
